5831766f9b61b96d05bd86ae10aa65a035772582
RedwanAbdullah387 <redwanabdullah7118@gmail.com>
2024-04-03 08:49:14 +0600

    Needed to change url and views since new htmls added. url still needs fixing

--- homepage/views.py
+++ homepage/views.py
@@ -8,15 +8,15 @@ def home(request):
 
 
 def signUp(request):
-    return render(request, template_name='signUp.html')
+    return render(request, template_name='sign up.html')
 
-
-def feed(request):
-    return render(request, template_name='feed.html')
+# feed removed
+# def feed(request):
+    # return render(request, template_name='feed.html')
 
 
 def ideasHub(request):
-    return render(request, template_name='ideasHub.html')
+    return render(request, template_name='ideas hub.html')
 
 
 def profile(request):
@@ -24,4 +24,32 @@ def profile(request):
 
 
 def templatesLib(request):
-    return render(request, template_name='templatesLib.html')
+    return render(request, template_name='templates library page.html')
+
+
+def AccStatus(request):
+    return render(request, template_name='account status.html')
+
+
+def activityLog(request):
+    return render(request, template_name='activity log.html')
+
+
+def logIn(request):
+    return render(request, template_name='log in.html')
+
+
+def moderation(request):
+    return render(request, template_name='moderation.html')
+
+
+def orderAndPayments(request):
+    return render(request, template_name='orders and payments.html')
+
+
+def settings(request):
+    return render(request, template_name='settings.html')
+
+
+def updateProfile(request):
+    return render(request, template_name='Update profile.html')
